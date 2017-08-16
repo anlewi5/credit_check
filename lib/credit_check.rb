d9408a37aa7a300d18439a2c60946dbe0bef64e1
Anna Lewis <anna.noel.lewis@gmail.com>
2017-08-16 07:57:44 -0600

    Make credit_check pass tests

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -5,28 +5,30 @@ class CardChecker
   attr_reader :card_number
 
   def initialize(card_number)
-    @@card_number = card_number
+    @card_number = card_number
   end
 
   #Returns an array of strings representing digits in reverse order
   def reverse_card_number
-    @@reversed_card = @@card_number.to_s.reverse.each_char.to_a
+    @reversed_card = @card_number.to_s.reverse.each_char.to_a
   end
 
   def convert_digit(digit)
     if digit.to_i >= 10
       digit = digit[0].to_i + digit[1].to_i
       digit = digit.to_s
+      return digit
+    else
+      return digit
     end
-    digit
   end
 
   #Returns string array with odd indices doubled
   def make_doubles
-      @@doubled_card =
-        @@reversed_card.map.with_index do |digit, index|
+      @doubled_card =
+        @reversed_card.map.with_index do |digit, index|
           if index.odd?
-            (digit.to_i * 2).to_s
+            digit = (digit.to_i * 2).to_s
             convert_digit(digit)
           else
             digit
@@ -35,17 +37,18 @@ class CardChecker
   end
 
   def sum_digits
-    @@card_sum = 0
-    @@doubled_card.each do |digit|
-      @@card_sum += digit.to_i
+    @card_sum = 0
+    @doubled_card.each do |digit|
+      @card_sum += digit.to_i
     end
+    @card_sum
   end
 
   def check_sum
-    if @@card_sum % 10 === 0
-      puts "The number is valid!"
+    if @card_sum % 10 === 0
+      "The number is valid!"
     else
-      puts "The number is invalid!"
+      "The number is invalid!"
     end
   end
 
@@ -56,6 +59,3 @@ class CardChecker
     check_sum
   end
 end
-
-card_test = CardChecker.new("4929735477250543")
-card_test.check_card
--- test/credit_check_test.rb
+++ test/credit_check_test.rb
@@ -1,13 +1,14 @@
 gem 'minitest', '~> 5.2'
 require 'minitest/autorun'
 require 'minitest/pride'
-require_relative 'credit_check'
+require_relative '../lib/credit_check'
 
 class CardCheckerTest < Minitest::Test
   def test_it_reverses_card_number
     card_number = CardChecker.new("4929735477250543")
     assert_equal ["3", "4", "5", "0", "5", "2", "7", "7", "4", "5", "3", "7", "9", "2", "9", "4"], card_number.reverse_card_number
-  end
+  end           #["3", "4", "5", "0", "5", "2", "7", "7", "4", "5", "3", "7", "9", "2", "9", "4"]
+                #"3"0 "4"1 "5"2 "0"3 "5"4 "2"5 "7"6 "7"7 "4"8 "5"9 "3"10 "7"11 "9"12 "2"13 "9"14 "4"15 digits and index values
 
   def test_it_doubles_every_second_digit
     card_number = CardChecker.new("4929735477250543")
